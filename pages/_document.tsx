b1ab8040f0d3b5c7b959197d956dd4bba3a916f8
Giovani <giovanigonzalezchavez@gmail.com>
2023-01-18 14:08:22 -0600

    [TEST] analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang='es'>
       <Head />
       <body>
+        <script data-host="http://seo.aythen.com" data-dnt="false" src="https://seo.aythen.com/js/script.js" id="ZwSg9rf6GA" async defer></script>
         <Main />
         <NextScript />
       </body>
